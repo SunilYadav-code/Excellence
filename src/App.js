e79dc1b094040fd2583e151784219798ce6773c7
SunilYadav-code <rocky.sunil97@gmail.com>
2021-06-22 20:19:58 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 const { create } = require("domain");
 const mongoose= require("mongoose");
-//const validator=require("validator");
+
 mongoose.connect("mongodb://localhost:27017/Excellence1",{useNewUrlParser:true,useUnifiedTopology:true,useCreateIndex:true}).then(()=>console.log("connection successful..."))
 .catch((err)=>console.log(err));
 
@@ -27,23 +27,8 @@ const condidateSchema=new mongoose.Schema({
         required: true
     }
 })
-// const testSchema=new mongoose.Schema({
-//     firstRound:{
-//         type:Number,
-//         required: true
-//     },
-//     secondRound:{
-//         type:Number,
-//         required: true
-//     },
-//     thirdRound:{
-//         type:Number,
-//         required: true
-//     }
-// })
 
 const Condidate=new mongoose.model("Condidate",condidateSchema);
-//const Testscore=new mongoose.model("Testscore",testSchema);
 
 const createDocument=async()=>{
     try{
@@ -68,24 +53,17 @@ const createDocument=async()=>{
             secondRound:8,
             thirdRound:10,
         })
-        //const newTestscore=new Testscore({
-          // firstRound:10,
-           //secondRound:10,
-           //thirdRound:10
-        //})
+        
             
         
         const result=await Condidate.insertMany([sunil1,sunil2,sunil3]);
-        //const result1=await newTestscore.save();
+        
         console.log(result);
-        //console.log(result1);
+        
 
     }catch(err){
         console.log(err);
-    }
-   
+    }  
       
 }
-
-
 createDocument();
